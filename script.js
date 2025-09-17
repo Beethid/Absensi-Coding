19d0d1ca2872904d21c067672c2082a17207352b
unknown <nonamemia301@gmail.com>
2025-09-18 03:08:27 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+
+let radios = document.querySelectorAll('input[name="myOption"]');
+let alasan = document.getElementById("alasan");
+
+radios.forEach(radio => {
+  radio.addEventListener("change", function() {
+    if (this.value !== "Hadir" && this.value != 'Sakit') {
+      alasan.classList.add("alasan");
+    } else {
+      alasan.classList.remove("alasan"); 
+    }
+  });
+});
+
+
+function tombol(){
+    alert("Terkirim")
+}
